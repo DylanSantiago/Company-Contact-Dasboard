7a0285a4a6d875b87a77a2c81384ac50487dbb45
DylanSantiago <2dylansantiago@gmail.com>
2022-11-02 04:29:30 -0400

    index update

--- index.js
+++ index.js
@@ -3,26 +3,65 @@ const Engineer= require("./Develop/lib/engineer");
 const Intern = require("./Develop/lib/intern");
 const inquirer = require("inquirer")
 
+const staff = [];
+
 function promptManager () {
     inquirer
-        .prompt([
-            {
-                type: 'input',
-                name: 'name',
-                message: '',
-            },
-            {
-                type: 'input',
-                name: 'name',
-                message: '',
-            },
-            {
-                type: 'input',
-                name: 'name',
-                message: '',
-            },
-        ])
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Hello Manager, What is your name?',
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'What is your employee ID #?',
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is your email?',
+        },
+        {
+            type: 'input',
+            name: 'office',
+            message: 'What is your office #?',
+        }
+    ])
+    .then(({name, id, email, office}) => {
+        this.Manager = new Manager(name, id, email, office);
+        staff.push(this.Manager)
+        getRole();
+    })
 }
+
+function getRole() {
+    inquirer
+    .prompt([
+        {
+            type: 'list',
+            name: 'staff',
+            message:'Would you like to add more Team Members?',
+            choices: [
+                'Engineer',
+                'Intern',
+                'No'
+            ]
+        }
+    ])
+    .then (({add}) => {
+        if(add === 'Engineer') {
+            addEngineer();
+        }
+        else if (add === 'Intern') {
+            addIntern
+        }
+        else if (add === 'No') {
+
+        }
+    })
+};
 
 function addEngineer () {
     inquirer
@@ -30,25 +69,30 @@ function addEngineer () {
         {
             type: 'input',
             name: 'name',
-            message: '',
+            message: 'What is the Engineers name?',
         },
         {
             type: 'input',
-            name: 'name',
-            message: '',
+            name: 'id',
+            message: 'What is this Engineers employee ID #?',
         },
         {
             type: 'input',
-            name: 'name',
-            message: '',
+            name: 'email',
+            message: 'What is the Engineers email?',
         },
         {
             type: 'input',
-            name: 'name',
-            message: '',
+            name: 'github',
+            message: 'What is the Engineers Github account?',
         },
     ])
-}
+    .then(({name, id, email, github}) => {
+        this.Engineer = new Engineer(name, id, email, github);
+        staff.push(this.Engineer)
+        getRole();
+    })
+};
 
 function addIntern () {
     inquirer
@@ -56,23 +100,32 @@ function addIntern () {
         {
             type: 'input',
             name: 'name',
-            message: '',
+            message: 'What is the interns name?',
         },
         {
             type: 'input',
-            name: 'name',
-            message: '',
+            name: 'id',
+            message: 'What is the Interns ID #?',
         },
         {
             type: 'input',
-            name: 'name',
-            message: '',
+            name: 'email',
+            message: 'What is the Interns email?',
         },
         {
             type: 'input',
-            name: 'name',
-            message: '',
+            name: 'school',
+            message: 'Where does your intern go to school?',
         },
     ])
+    .then(({name, id, email, school}) => {
+        this.Intern = new Intern(name, id, email, school);
+        staff.push(this.Intern)
+        getRole();
+    })
+};
+
+function addStaff () {
+
 }
 
