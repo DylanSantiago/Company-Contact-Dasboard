92975b8fa4d560dcdaf7539e5a1e81076771cf49
DylanSantiago <2dylansantiago@gmail.com>
2022-11-05 16:23:22 -0400

    initial css and bootstrap foundation laid

--- Develop/lib/generatehtml.js
+++ Develop/lib/generatehtml.js
@@ -6,18 +6,20 @@ function displayHTML (displayCards) {
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Company Contact Dashboard</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" 
+        rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     </head>
     <body>
-        <section class =" "> 
-            <section class = " ">
-                <div class = " ">
-                    <h1 class = " "></h1>
+        <section class ="container-fluid"> 
+            <section class = "row">
+                <div class = "mt-4 p-5 bg-primary text-white">
+                    <h1 class = "text-center">My Team</h1>
                 </div>
             </section>
-        </section
+        </section>
 
-        <section class =" ">
-            <div class = "">
+        <section class ="container col-12 p-3">
+            <div class = "row d-flex justify content-center">
 
                 ${displayCards}
 
@@ -58,16 +60,16 @@ function displayStaff (roster) {
 
 function promptManager(manager) {
     return `
-    <section class = " ">
-        <div class = " ">
+    <section class = "card col-3 m-3">
+        <div class = "card-header">
             <h2>${manager.name}</h2>
             <h3> Manager </h3>
-        </div
-        <div class = " ">
-            <ul class = " ">
-                <li class = " "> Employee ID: ${manager.id}</li>
-                <li class = " "> Email: <a href="mailto:${manager.email}">${manager.email}</a></li>
-                <li class = " "> Office Number: ${manager.officeNumber}</li>
+        </div>
+        <div class = "card-body bg-primary ">
+            <ul class = "list-group">
+                <li class = "list-group-item"> Employee ID: ${manager.id}</li>
+                <li class = "list-group-item"> Email: <a href="mailto:${manager.email}">${manager.email}</a></li>
+                <li class = "list-group-item"> Office Number: ${manager.officeNumber}</li>
             </ul>
         </div>
     </section>
@@ -76,16 +78,16 @@ function promptManager(manager) {
 
 function addEngineer(engineer) {
     return `
-    <section class = " ">
-        <div class = " ">
+    <section class = "card col-3 m-3">
+        <div class = "card-header">
             <h2>${engineer.name}</h2>
             <h3> Engineer </h3>
-        </div
-        <div class = " ">
-            <ul class = " ">
-                <li class = " "> Employee ID: ${engineer.id}</li>
-                <li class = " "> Email: <a href="mailto:${engineer.email}">${engineer.email}</a></li>
-                <li class = " "> Github: <a href="https://github.com/${engineer.github}">${engineer.github}</a></li>
+        </div>
+        <div class = "card-body bg-primary">
+            <ul class = "list-group">
+                <li class = "list-group-item"> Employee ID: ${engineer.id}</li>
+                <li class = "list-group-item"> Email: <a href="mailto:${engineer.email}">${engineer.email}</a></li>
+                <li class = "list-group-item"> Github: <a href="https://github.com/${engineer.github}">${engineer.github}</a></li>
             </ul>
         </div>
     </section>
@@ -94,16 +96,16 @@ function addEngineer(engineer) {
 
 function addIntern(intern) {
     return `
-    <section class = " ">
-        <div class = " ">
-            <h2>${intern.name}</h2>
-            <h3> Intern </h3>
-        </div
-        <div class = " ">
-            <ul class = " ">
-                <li class = " "> Employee ID: ${intern.id}</li>
-                <li class = " "> Email: <a href="mailto:${intern.email}">${intern.email}</a></li>
-                <li class = " "> School: ${intern.school}</li>
+    <section class = "card col-3 m-3">
+        <div class = "card-header">
+            <h2 class = "card-title">${intern.name}</h2>
+            <h3 class = "card-title"> Intern </h3>
+        </div>
+        <div class = "card-body bg-primary">
+            <ul class = "list-group">
+                <li class = "list-group-item"> Employee ID: ${intern.id}</li>
+                <li class = "list-group-item"> Email: <a href="mailto:${intern.email}">${intern.email}</a></li>
+                <li class = "list-group-item"> School: ${intern.school}</li>
             </ul>
         </div>
     </section>
