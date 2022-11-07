dbf1db3819d45dde1c95af2b64254cac610ef3eb
DylanSantiago <2dylansantiago@gmail.com>
2022-11-07 13:09:52 -0500

    small grammatical error in generatehtml.js fixed

--- Develop/lib/generatehtml.js
+++ Develop/lib/generatehtml.js
@@ -10,7 +10,7 @@ function displayHTML (displayCards) {
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" 
         rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     </head>
-    <body class ="bg-secondary"
+    <body class ="bg-secondary">
         <section class ="container-fluid"> 
             <section class = "row">
                 <div class = "mt-4 p-5 bg-success text-white">
